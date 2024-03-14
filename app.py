15fbd82894786caec6428b7d4e16dabaa55ab762
coldcanuk <chuck.pitre@hotmail.com>
2024-03-14 14:25:17 -0400

    debug_mode True we need more data

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ assistant_id_p = str(ASSISTANT_PENELOPE)
 ASSISTANT_MARIECAISSIE = os.getenv('ASSISTANT_MARIECAISSIE')
 assistant_id_mc = str(ASSISTANT_MARIECAISSIE)
 
+
 # Create the OpenAI client
 client = OpenAI(api_key=OPENAI_API_KEY)
 

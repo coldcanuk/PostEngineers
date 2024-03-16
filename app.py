6e654d3d519b73aad8e1618c29c48b285f5da157
coldcanuk <chuck.pitre@hotmail.com>
2024-03-15 23:03:27 -0400

    17

--- app.py
+++ app.py
@@ -130,11 +130,13 @@ async def post(ctx, message: str):
         foo.append(reply_text)
         #intCount2 = intCount2 + 1
     logger.debug(f"sent text to Discord. Now setting combined_text as the user prompt for Marie Caissie")
-    #insight, masterpiece = extract_insight_and_masterpiece(reply_texts) # Sends the direct 'value' content to be parse for Marie Caissie
-    #combined_text = f"My dearest Marie Caissie. I require your talents. It is with the greatest urgency that I need your artistic brilliance to compose for us a useable image prompt intended for use with an AI image generator. I thought long and hard about this and here is the insight I used Insight: {insight} TO DEVELOP my masterpiece Post Masterpiece: {masterpiece}"
-    combined_text = f"DEBUG DEBUG DEBUG"
     logger.debug(f"reply_text's type is: {type(reply_texts)}")
     logger.debug(f"foo's type is: type{type(foo)}")
+    insight, masterpiece = extract_insight_and_masterpiece(reply_texts) # Sends the direct 'value' content to be parse for Marie Caissie
+    #combined_text = f"My dearest Marie Caissie. I require your talents. It is with the greatest urgency that I need your artistic brilliance to compose for us a useable image prompt intended for use with an AI image generator. I thought long and hard about this and here is the insight I used Insight: {insight} TO DEVELOP my masterpiece Post Masterpiece: {masterpiece}"
+    combined_text = f"DEBUG DEBUG DEBUG"
+    logger.debug(f"{len(insight)}")
+    logger.debug(f"{len(masterpiece)}")
     #logger.debug(f"the value of insight is: {insight}")
     #logger.debug(f"the value of masterpiece is: {masterpiece}")
     #combined_text = f"My dearest Marie Caissie. I require your talents. It is with the greatest urgency that I need your artistic brilliance to compose for us a useable image prompt intended for use with an AI image generator. I thought long and hard about this and here is the insight I used Insight: {insight} TO DEVELOP my masterpiece Post Masterpiece: {masterpiece}"

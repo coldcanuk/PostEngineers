f3ea4dcea9d2f92160b1db6219c25a2974546a7b
coldcanuk <chuck.pitre@hotmail.com>
2024-03-15 20:06:00 -0400

    test 030

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ async def post(ctx, message: str):
         await ctx.followup.send(reply_text)  # Sends the direct 'value' content
         #intCount2 = intCount2 + 1
     logger.debug(f"sent text to Discord. Now setting combined_text as the user prompt for Marie Caissie")
-    insight, masterpiece = extract_insight_and_masterpiece(reply_texts) # Sends the direct 'value' content to be parse for Marie Caissie
+    #insight, masterpiece = extract_insight_and_masterpiece(reply_texts) # Sends the direct 'value' content to be parse for Marie Caissie
     combined_text = f"My dearest Marie Caissie. I require your talents. It is with the greatest urgency that I need your artistic brilliance to compose for us a useable image prompt intended for use with an AI image generator. I thought long and hard about this and here is the insight I used Insight: {insight} TO DEVELOP my masterpiece Post Masterpiece: {masterpiece}"
     # This combined_text is ready to be sent to Marie Caissie for further processing.
     # Example: await handle_post_command(combined_text, assistant_id_mc, mariecaissie_instructions)

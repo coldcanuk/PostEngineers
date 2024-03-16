c68a3e259cab72bf95f5bc3afd0172ad1dbb100a
coldcanuk <chuck.pitre@hotmail.com>
2024-03-16 08:20:53 -0400

    23

--- app.py
+++ app.py
@@ -127,8 +127,8 @@ async def post(ctx, message: str):
     for reply_text in reply_texts:
         #logger.debug(f"Reply iteration: {intCount2} ")
         await ctx.followup.send(reply_text)  # Sends the direct 'value' content
-        print(reply_text)
-        foo.append(reply_text)
+        await print(reply_text)
+        await foo.append(reply_text)
         #intCount2 = intCount2 + 1
     
     logger.debug(f"sent text to Discord. Now setting combined_text as the user prompt for Marie Caissie")

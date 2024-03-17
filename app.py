e73128d07c69c03989302f736c233679aff3e72d
coldcanuk <chuck.pitre@hotmail.com>
2024-03-17 19:15:06 -0400

    38

--- app.py
+++ app.py
@@ -141,16 +141,17 @@ async def post(ctx, message: str):
         logger.debug(f" reply_text had something and we appended it to debug_object_reply")
     logger.debug(f"sent text to Discord. Now setting combined_text as the user prompt for Marie Caissie")
     logger.debug(f"reply_text's type is: {type(reply_texts)}")
-    logger.debug(f"foo's type is: type{type(debug_object_reply)}")
+    logger.debug(f"debug_object_reply's type is: type{type(debug_object_reply)}")
     logger.info(f"Creating our custom object")
     insight = "Insight: 1 DEBUG SET"
     masterpiece = "Masterpiece: 2 DEBUG SET"
-    combined_text = "I am DEBUG combined_text of" + insight + "and the " + masterpiece
-    logger.info(f"{len(insight)}")
-    logger.info(f"{len(masterpiece)}")
+    logger.info(f"Insight's length is: {len(insight)}")
+    logger.info(f"Masterpiece's length is: {len(masterpiece)}")
+    combined_text = "I am DEBUG combined_text of" + insight + " and the " + masterpiece
     logger.info(f"Prepared for Marie Caissie, the output of combined_text is: {combined_text}")
-    insight, masterpiece = extract_insight_and_masterpiece(combined_text) # Sends the direct 'value' content to be parse for Marie Caissie
-    
+    insight, masterpiece = extract_insight_and_masterpiece(debug_reply_texts) # Sends the direct 'value' content to be parse for Marie Caissie
+    logger.info(f"new Insight's length is: {len(insight)}")
+    logger.info(f"new Masterpiece's length is: {len(masterpiece)}")
   
     '''data_dict = {}
     for line in debug_object_reply:

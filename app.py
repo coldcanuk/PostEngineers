088285f44181f9a4549fa4a27852399e83220d1d
coldcanuk <chuck.pitre@hotmail.com>
2024-04-15 08:51:45 -0400

    redo 2

--- app.py
+++ app.py
@@ -82,8 +82,8 @@ async def post(ctx, message: str):
     # Penelope's create_and_run thread with openAI
     try:
       start_createandrun(message, assistant_id_p)
-      strThreadID=start_createandrun.strThreadID
-      strResponseID=start_createandrun.strResponseID
+      strThreadID=str(strThreadID)
+      strResponseID=str(strResponseID)
       logger.debug(f"return strThreadID:  {strThreadID}  and strResponseID:  {strResponseID}")
     except Exception as e:
       await ctx.followup.send("Zap. Failed to start the create and run function")
